73101055e4759df802ff97a98fb622b01f22d8bc
Patrick Minda <patrick.minda@hotmail.com>
2024-12-07 14:25:14 +1100

    Update: Calculate revenue function implemented.

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,24 @@ interface Record {
 }
 
 // Load and parse the data file
-const data: Record[] = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+const generalLedgerData = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+const records: Record[] = generalLedgerData.data;
+
+const calculateRevenue = (records: Record[]) => {
+  let revenue = 0;
+  records.map((record) => {
+    if (record.account_category === 'revenue') revenue += record.total_value;
+  });
+  return revenue;
+};
 
 const main = () => {
   console.log('index.js');
+  console.log(records);
+
+  const totalRevenue = calculateRevenue(records);
+
+  console.log(`Total Revenue: ${totalRevenue}`);
 };
 
 main();
